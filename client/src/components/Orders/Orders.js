6b0bfb64647ceca373a8278b4b622f82bbd8dc9a
Ice Chidi <icechidi3@gmail.com>
2025-04-21 23:59:16 +0300

    Orders.js contents updated and js portion integrated

--- client/src/components/Orders/Orders.js
+++ client/src/components/Orders/Orders.js
@@ -201,6 +201,7 @@ function NewOrder() {
               onClick={() => removeOrderItem(index)}
             >
               Remove Item
+              
             </button>
           </div>
         ))}
